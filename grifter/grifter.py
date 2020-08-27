4d355e034fe2f0d35ccb039aa0ee232d32037869
Justin Chadwell <me@jedevc.com>
2020-08-27 19:10:37 +0100

    Enter debug mode when no target specified

--- grifter/grifter.py
+++ grifter/grifter.py
@@ -2,7 +2,7 @@ import argparse
 
 import face_recognition
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw
 
 from .utils import gif
 
@@ -11,19 +11,20 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--infile", required=True)
     parser.add_argument("-o", "--outfile", required=True)
-    parser.add_argument("-t", "--target", required=True)
+    parser.add_argument("-t", "--target", default=None)
     parser.add_argument("-s", "--scale", type=float, default=1)
-    parser.add_argument("--frames", type=int, default=None)
+    parser.add_argument("--frames", type=int, default=0)
     parser.add_argument("--upscale", type=int, default=0)
     parser.add_argument("--model", choices=["cnn", "hog"], default="hog")
     args = parser.parse_args()
 
-    if args.frames is not None and args.frames <= 0:
-        print("[*] no frames to process, exiting")
-        return
-
     im = Image.open(args.infile)
-    target = Image.open(args.target)
+
+    if args.target:
+        target = Image.open(args.target)
+    else:
+        print("[*] INFO: no target specified, using debug rectangle")
+        target = None
 
     outputs = []
     for i, frame in enumerate(gif.get_frames(im)):
@@ -41,7 +42,7 @@ def main():
         output = process_frame(frame, faces, target, args.scale)
         outputs.append(output)
 
-        if args.frames is not None and len(outputs) >= args.frames:
+        if args.frames and len(outputs) >= args.frames:
             break
 
     print(f"\r[*] processed {len(outputs)} frames total")
@@ -50,23 +51,32 @@ def main():
     outputs[0].save(args.outfile, save_all=True, append_images=outputs[1:], repeat=0)
 
 
-def process_frame(frame, faces, target, scale):
+def process_frame(frame, faces, target=None, scale=1):
     layer = Image.new("RGBA", frame.size)
+    draw = ImageDraw.Draw(layer)
     for face in faces:
         top, left, bottom, right = face
         width = right - left
         height = bottom - top
-        if width > height:
-            factor = target.width / width
+
+        if target:
+            if width > height:
+                factor = target.width / width
+            else:
+                factor = target.height / height
+
+            fx = int(target.width / factor * scale)
+            fy = int(target.height / factor * scale)
+            target = target.resize((fx, fy))
+
+            ox = left - (target.width - width) // 2
+            oy = top - (target.height - height) // 2
+            layer.paste(target, (ox, oy), target)
         else:
-            factor = target.height / height
-
-        fx = int(target.width / factor * scale)
-        fy = int(target.height / factor * scale)
-        target = target.resize((fx, fy))
-
-        ox = left - (target.width - width) // 2
-        oy = top - (target.height - height) // 2
-        layer.paste(target, (ox, oy), target)
+            size = max(width, height) * scale
+            
+            ox = left - (size - width) // 2
+            oy = top - (size - height) // 2
+            draw.rectangle([(ox, oy), (ox + size, oy + size)], outline='red', width=2)
 
     return Image.alpha_composite(frame, layer)
